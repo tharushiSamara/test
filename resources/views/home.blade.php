61d97ab046c7dd951fe28b6a5772420c38567c04
MohamedSabthar <sabtharugc0@gmail.com>
2019-09-01 23:01:51 +0530

    layout file created

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,31 @@
 @extends('layouts.app')
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
+@section('title','Dashboard')
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
+@section('sidebar')
+<li class="nav-item">
+        <a class="nav-link " href="./examples/icons.html">
+          <i class="ni ni-planet text-blue"></i> Icons
+        </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link " href="./examples/maps.html">
+          <i class="ni ni-pin-3 text-orange"></i> Maps
+        </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link " href="./examples/profile.html">
+          <i class="ni ni-single-02 text-yellow"></i> User profile
+        </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link " href="./examples/tables.html">
+          <i class="ni ni-bullet-list-67 text-red"></i> Tables
+        </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="./examples/login.html">
+          <i class="ni ni-key-25 text-info"></i> Login
+        </a>
+      </li>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -13,5 +13,7 @@
 
 Route::get('/', 'Auth\LoginController@showLoginForm')->name('root');  //site root shows the login form
 Auth::routes(['verify' => true]);                                     //authentication routes with email verification
-
+Route::get('/a',function(){
+    return view('layouts.app');
+});
 Route::get('/home', 'HomeController@index')->name('home');
