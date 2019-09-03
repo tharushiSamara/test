e7a586528e7b0d1f02e2332355b9d1b345e606b6
MohamedSabthar <sabtharugc0@gmail.com>
2019-09-03 15:08:44 +0530

    added datatables and configured bootstarp for datatables

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -314,30 +314,8 @@
       {{-- End of header --}}
       
       {{-- Page content --}}
-      <div class="container-fluid mt--7">
-        <div class="row">
-          <div class="col">
-            
-            <div class="card shadow">
-              <div class="card-header bg-transparent">
-                <h3 class="mb-0">Icons</h3>
-              </div>
-              <div class="card-body">
-                <div class="row icon-examples">
-                  <div class="col-lg-3 col-md-6">
-                    <button type="button" class="btn-icon-clipboard" data-clipboard-text="active-40" title="Copy to clipboard">
-                      <div>
-                        <i class="ni ni-active-40"></i>
-                        <span>active-40</span>
-                      </div>
-                    </button>
-                  </div>
-                </div>
-              </div>
-            </div>
-          
-          </div>
-        </div>
+      @yield('pageContent')
+      
 
         {{-- Footer --}}
         <footer class="footer">
